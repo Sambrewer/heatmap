7471207e02bf8e8a6579482322eb8c91aaf37dd1
Sam Brewer <Sam Brewer>
2017-06-26 12:55:44 -0600

    filter working and added styling

--- app/scripts.js
+++ app/scripts.js
@@ -9,25 +9,42 @@ $(document).ready(() => {
     });
 
     heatmap = new google.maps.visualization.HeatmapLayer({
-      data:coordinates,
+      data: coordinates,
       map: map
-    });
-    return false;
-  }), 300)
+    })
+
+  }), 500)
 
   let paramId;
   let getPoints = (id) => {
-    console.log(id);
+    (id === undefined) ? id = 4 : id = id;
+    switch (id) {
+      case '4':
+        document.getElementById('dataType').innerHTML = 'Coins'
+        break;
+      case '5':
+        document.getElementById('dataType').innerHTML = 'Keys'
+        break;
+      case '637':
+        document.getElementById('dataType').innerHTML = 'Cinemark Prizes'
+        break;
+    }
     jQuery.ajax(`https://staging.seek.fit/api/challenge/?loot_type=${id}`).done((response) => {
       coordinates = response.features[0].geometry.coordinates.map((cord, ind) => {
-        return {location: (new google.maps.LatLng(cord[1], cord[0])), weight: 3}
+        return {location: (new google.maps.LatLng(cord[1], cord[0])), weight: 1000}
       });
+
     })
   }
   getPoints($('#rewardsSelect').val());
   $(`#rewardsForm`).submit(() => {
+
     getPoints($('#rewardsSelect').val())
-    initMap();
+    heatmap.setMap(null);
+    heatmap = new google.maps.visualization.HeatmapLayer({
+      data: coordinates,
+      map: map
+    })
     return false;
   })
 })
